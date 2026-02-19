9b084ae04001f4156330e095d888fe4c6c807b2f
Balla Saradhi <saradhi0213@gmail.com>
2026-02-19 18:54:50 +0530

    Added LC 1295 Find Numbers with Even Number of Digits (Digit count using division)

---
+++ src/arrays/LC_1295_Find_Numbers_with_Even_Number_of_Digits.java
@@ -0,0 +1,42 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class LC_1295_Find_Numbers_with_Even_Number_of_Digits {
+
+    public int findNumbers(int[] nums) {
+
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int num = nums[i];
+            int count = 0;
+
+            while (num > 0) {
+                count++;
+                num = num / 10;
+            }
+
+            if (count % 2 == 0) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        // Hardcoded test case
+        int[] nums = {12, 345, 2, 6, 7896};
+
+        LC_1295_Find_Numbers_with_Even_Number_of_Digits obj =
+                new LC_1295_Find_Numbers_with_Even_Number_of_Digits();
+
+        int result = obj.findNumbers(nums);
+
+        System.out.println("Input:  " + Arrays.toString(nums));
+        System.out.println("Count of Even-Digit Numbers: " + result);
+    }
+}
