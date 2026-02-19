6efe609dcbf64f0148d4a723cda78cc31d3b5aaf
Balla Saradhi <saradhi0213@gmail.com>
2026-02-19 19:02:33 +0530

    Added LC 344 Reverse String (Two Pointer | In-place | TC O(n))

---
+++ src/arrays/LC_344_Reverse_String.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class LC_344_Reverse_String {
+
+    public void reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Hardcoded test case
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+
+        LC_344_Reverse_String obj =
+                new LC_344_Reverse_String();
+
+        System.out.println("Before: " + Arrays.toString(s));
+
+        obj.reverseString(s);
+
+        System.out.println("After:  " + Arrays.toString(s));
+    }
+}
